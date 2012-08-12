cbb8eee2b563b1b20d9c49ec2eccd03a9a7ce994
papp.robert.s@gmail.com <papp.robert.s@gmail.com@bdd231bc-1b6a-c87e-4825-1738cc19bc25>
2012-08-12 23:13:18 +0000

    Cineworld [ADD] Display all details of the cinema

--- android/src/com/twister/cineworld/ui/activity/CinemaActivity.java
+++ android/src/com/twister/cineworld/ui/activity/CinemaActivity.java
@@ -1,7 +1,14 @@
 package com.twister.cineworld.ui.activity;
 
+import java.net.URLEncoder;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.*;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 import com.twister.cineworld.R;
 import com.twister.cineworld.model.json.CineworldAccessor;
@@ -30,5 +37,24 @@ public class CinemaActivity extends Activity implements Retriever<CineworldCinem
 
 	public void update(final CineworldCinema result) {
 		setTitle(String.format("Cinema details: %s", result.getName()));
+		TextView name = (TextView) findViewById(R.id.cinema_name);
+		TextView address = (TextView) findViewById(R.id.cinema_address);
+		TextView url = (TextView) findViewById(R.id.cinema_url);
+		TextView phone = (TextView) findViewById(R.id.cinema_phone);
+
+		name.setText(String.format("%s (%d)", result.getName(), result.getId()));
+
+		// TODO find a nicer way of handlink address links
+		address.setText(String.format("%s, %s", result.getAddress(), result.getPostcode()));
+		address.setOnClickListener(new OnClickListener() {
+			public void onClick(final View v) {
+				String query = String.format("%2$s (%1$s)", result.getAddress(), result.getPostcode());
+				query = URLEncoder.encode(query);
+				String uri = String.format("geo:0,0?q=%s", query);
+				startActivity(new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(uri)));
+			}
+		});
+		url.setText(result.getCinemaUrl());
+		phone.setText(result.getTelephone());
 	}
 }
