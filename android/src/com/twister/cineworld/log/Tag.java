92e659b9546b2fd30a6ab5379680f2bbbd9610e3
zoltan.kiss.jr <zoltan.kiss.jr@bdd231bc-1b6a-c87e-4825-1738cc19bc25>
2012-08-27 20:47:01 +0000

    Cineworld [COM] Tag enum [ADD] Tag string length check

--- android/src/com/twister/cineworld/log/Tag.java
+++ android/src/com/twister/cineworld/log/Tag.java
@@ -1,13 +1,36 @@
 package com.twister.cineworld.log;
 
+import android.util.Log;
+
+/**
+ * Available tag strings in the application logs.
+ * 
+ * @author Zoltán Kiss
+ */
 public enum Tag {
+	/*
+	 * Bear in mind that the length of tags cannot be longer than 23 characters according to the Android API.
+	 */
 	JSON,
 	ACCESS,
 	UI,
 	GEO,
 	SYSTEM;
 
-	private static final String	TAG_PREFIX	= "cw.";
+	private static final int	TAG_MAX_LENGTH	= 23;
+
+	private static final String	TAG_PREFIX		= "cw.";
+
+	static {
+		/*
+		 * Check tag string lengths
+		 */
+		for (Tag tag : Tag.values()) {
+			if (tag.getTag().length() > Tag.TAG_MAX_LENGTH) {
+				Log.w(null, "Tag value is longer than 23 chars: " + tag.name() + "=" + tag.getTag());
+			}
+		}
+	}
 
 	private final String		m_tag;
 
