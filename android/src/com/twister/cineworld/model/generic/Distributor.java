59a19161d3e2ebe7d69668d7a472eeddd8a0a990
papp.robert.s@gmail.com <papp.robert.s@gmail.com@bdd231bc-1b6a-c87e-4825-1738cc19bc25>
2012-10-03 17:40:54 +0000

    Cineworld [COM] Remove sallang on generic model for Category, Distributor, Event, Performance (http://code.google.com/p/cineworld-extra/issues/detail?id=11)

--- android/src/com/twister/cineworld/model/generic/Distributor.java
+++ android/src/com/twister/cineworld/model/generic/Distributor.java
@@ -1,33 +1,9 @@
 package com.twister.cineworld.model.generic;
 
-import com.google.gson.annotations.SerializedName;
-
-/**
- * <p>
- * This query returns a list of unique distributors for the films programmed at Cineworld.
- * </p>
- * <p>
- * Example JSON:
- * 
- * <pre>
- * {
- * id: 523,
- * name: "Miramax"
- * }
- * </pre>
- * 
- * </p>
- */
 public class Distributor extends GenericBase {
-	@SerializedName("id")
 	private int		m_id;
-	@SerializedName("name")
 	private String	m_name;
 
-	/**
-	 * @return System id for the distributor. This is the value that should be used by when making calls to the API that
-	 *         accept a distributor value
-	 */
 	public int getId() {
 		return m_id;
 	}
@@ -36,9 +12,6 @@ public class Distributor extends GenericBase {
 		m_id = id;
 	}
 
-	/**
-	 * @return Full distributor name
-	 */
 	public String getName() {
 		return m_name;
 	}
