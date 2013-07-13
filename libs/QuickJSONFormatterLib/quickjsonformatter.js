5f4359f3fb9a585cd45e6f309ae160aab727050c
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2013-07-13 19:58:54 +0000

    [SVN] r581 QuickJSONFormatterLib in /Web/QuickJSONFormatterLib/ [REF] Function headers

--- libs/QuickJSONFormatterLib/quickjsonformatter.js
+++ libs/QuickJSONFormatterLib/quickjsonformatter.js
@@ -1,9 +1,9 @@
+(function() {
 // we need tabs as spaces and not CSS magin-left
 // in order to ratain format when coping and pasing the code
-window.SINGLE_TAB = "  ";
-window.ImgCollapsed = "images/Collapsed.gif";
-window.ImgExpanded = "images/Expanded.gif";
-window.QuoteKeys = true;
+var SINGLE_TAB = "  ";
+var ImgCollapsed = "images/Collapsed.gif";
+var ImgExpanded = "images/Expanded.gif";
 function $id(id){ return document.getElementById(id); }
 function IsArray(obj) {
 	return obj &&
@@ -11,7 +11,10 @@ function IsArray(obj) {
 					typeof obj.length === 'number' &&
 					!(obj.propertyIsEnumerable('length'));
 }
-function Process(){
+
+QuickJSONFormatter = (function() {
+var QuoteKeys = true;
+QuickJSONFormatter.prototype.Process = function() {
 	SetTab();
 	window.IsCollapsible = $id("CollapsibleView").checked;
 	var json = $id("RawJson").value;
@@ -25,10 +28,10 @@ function Process(){
 		alert("JSON is not well formated:\n"+e.message);
 		$id("Canvas").innerHTML = "";
 	}
-}
-window._dateObj = new Date();
-window._regexpObj = new RegExp();
-function ProcessObject(obj, indent, addComma, isArray, isPropertyContent){
+};
+var _dateObj = new Date();
+var _regexpObj = new RegExp();
+QuickJSONFormatter.prototype.ProcessObject = function(obj, indent, addComma, isArray, isPropertyContent) {
 	var html = "";
 	var comma = (addComma) ? "<span class='Comma'>,</span> " : "";
 	var type = typeof obj;
@@ -86,15 +89,15 @@ function ProcessObject(obj, indent, addComma, isArray, isPropertyContent){
 		html += FormatLiteral(obj.toString().split("\\").join("\\\\").split('"').join('\\"'), "\"", comma, indent, isArray, "String");
 	}
 	return html;
-}
-function FormatLiteral(literal, quote, comma, indent, isArray, style){
+};
+QuickJSONFormatter.prototype.FormatLiteral = function(literal, quote, comma, indent, isArray, style) {
 	if(typeof literal == 'string')
 		literal = literal.split("<").join("&lt;").split(">").join("&gt;");
 	var str = "<span class='"+style+"'>"+quote+literal+quote+comma+"</span>";
 	if(isArray) str = GetRow(indent, str);
 	return str;
-}
-function FormatFunction(indent, obj){
+};
+QuickJSONFormatter.prototype.FormatFunction = function(indent, obj) {
 	var tabs = "";
 	for(var i = 0; i < indent; i++) tabs += window.TAB;
 	var funcStrArray = obj.toString().split("\n");
@@ -103,54 +106,54 @@ function FormatFunction(indent, obj){
 		str += ((i==0)?"":tabs) + funcStrArray[i] + "\n";
 	}
 	return str;
-}
-function GetRow(indent, data, isPropertyContent){
+};
+QuickJSONFormatter.prototype.GetRow = function(indent, data, isPropertyContent) {
 	var tabs = "";
 	for(var i = 0; i < indent && !isPropertyContent; i++) tabs += window.TAB;
 	if(data != null && data.length > 0 && data.charAt(data.length-1) != "\n")
 		data = data+"\n";
 	return tabs+data;
-}
-function CollapsibleViewClicked(){
+};
+QuickJSONFormatter.prototype.CollapsibleViewClicked = function() {
 	$id("CollapsibleViewDetail").style.visibility = $id("CollapsibleView").checked ? "visible" : "hidden";
 	Process();
-}
+};
 
-function QuoteKeysClicked(){
+QuickJSONFormatter.prototype.QuoteKeysClicked = function() {
 	window.QuoteKeys = $id("QuoteKeys").checked;
 	Process();
-}
+};
 
-function CollapseAllClicked(){
+QuickJSONFormatter.prototype.CollapseAllClicked = function() {
 	EnsureIsPopulated();
 	TraverseChildren($id("Canvas"), function(element){
 		if(element.className == 'collapsible'){
 			MakeContentVisible(element, false);
 		}
 	}, 0);
-}
-function ExpandAllClicked(){
+};
+QuickJSONFormatter.prototype.ExpandAllClicked = function() {
 	EnsureIsPopulated();
 	TraverseChildren($id("Canvas"), function(element){
 		if(element.className == 'collapsible'){
 			MakeContentVisible(element, true);
 		}
 	}, 0);
-}
-function MakeContentVisible(element, visible){
+};
+QuickJSONFormatter.prototype.MakeContentVisible = function(element, visible) {
 	var img = element.previousSibling.firstChild;
 	if(!!img.tagName && img.tagName.toLowerCase() == "img"){
 		element.style.display = visible ? 'inline' : 'none';
 		element.previousSibling.firstChild.src = visible ? window.ImgExpanded : window.ImgCollapsed;
 	}
-}
-function TraverseChildren(element, func, depth){
+};
+QuickJSONFormatter.prototype.TraverseChildren = function(element, func, depth) {
 	for(var i = 0; i < element.childNodes.length; i++){
 		TraverseChildren(element.childNodes[i], func, depth + 1);
 	}
 	func(element, depth);
-}
-function ExpImgClicked(img){
+};
+QuickJSONFormatter.prototype.ExpImgClicked = function(img) {
 	var container = img.parentNode.nextSibling;
 	if(!container) return;
 	var disp = "none";
@@ -161,8 +164,8 @@ function ExpImgClicked(img){
 	}
 	container.style.display = disp;
 	img.src = src;
-}
-function CollapseLevel(level){
+};
+QuickJSONFormatter.prototype.CollapseLevel = function(level) {
 	EnsureIsPopulated();
 	TraverseChildren($id("Canvas"), function(element, depth){
 		if(element.className == 'collapsible'){
@@ -173,25 +176,25 @@ function CollapseLevel(level){
 			}
 		}
 	}, 0);
-}
-function TabSizeChanged(){
+};
+QuickJSONFormatter.prototype.TabSizeChanged = function() {
 	Process();
-}
-function SetTab(){
+};
+QuickJSONFormatter.prototype.SetTab = function() {
 	var select = $id("TabSize");
 	window.TAB = MultiplyString(parseInt(select.options[select.selectedIndex].value), window.SINGLE_TAB);
-}
-function EnsureIsPopulated(){
+};
+QuickJSONFormatter.prototype.EnsureIsPopulated = function() {
 	if(!$id("Canvas").innerHTML && !!$id("RawJson").value) Process();
-}
-function MultiplyString(num, str){
+};
+QuickJSONFormatter.prototype.MultiplyString = function(num, str) {
 	var sb =[];
 	for(var i = 0; i < num; i++){
 		sb.push(str);
 	}
 	return sb.join("");
-}
-function SelectAllClicked(){
+};
+QuickJSONFormatter.prototype.SelectAllClicked = function() {
 
 	if(!!document.selection && !!document.selection.empty) {
 		document.selection.empty();
@@ -216,10 +219,15 @@ function SelectAllClicked(){
 		range.select($id("Canvas"));
 	else
 		window.getSelection().addRange(range);
-}
-function LinkToJson(){
+};
+QuickJSONFormatter.prototype.LinkToJson = function() {
 	var val = $id("RawJson").value;
 	val = escape(val.split('/n').join(' ').split('/r').join(' '));
 	$id("InvisibleLinkUrl").value = val;
 	$id("InvisibleLink").submit();
-}
+};
+
+    return QuickJSONFormatter;
+})();
+
+}).call(this);
