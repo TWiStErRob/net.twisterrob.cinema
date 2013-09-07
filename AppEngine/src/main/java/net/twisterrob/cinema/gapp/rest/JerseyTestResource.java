06ce748da6ed882b6246062fb51bfc29367306a4
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2013-09-07 23:47:20 +0000

    [SVN] r656 Cinema - AppEngine in /Projects/Cinema/ [EXP] Return a key properly marshalled

--- AppEngine/src/main/java/net/twisterrob/cinema/gapp/rest/JerseyTestResource.java
+++ AppEngine/src/main/java/net/twisterrob/cinema/gapp/rest/JerseyTestResource.java
@@ -1,19 +1,34 @@
 package net.twisterrob.cinema.gapp.rest;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-
-import org.glassfish.jersey.server.JSONP;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import com.google.appengine.api.datastore.*;
 
 @Path("/test")
 public class JerseyTestResource {
 	@GET
-	@JSONP(callback = "callback", queryParam = "callback")
-	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON, "application/javascript"})
-	public Key test() {
+	@Produces(MediaType.APPLICATION_JSON)
+	public KeyHolder test() {
 		Key parent = KeyFactory.createKey("SubKey", 12345);
 		Key child = KeyFactory.createKey(parent, "SuperKey", "KeyName");
-		return child;
+		return new KeyHolder(child);
+	}
+
+	private static class KeyHolder {
+		@XmlElement(namespace = "http://appengine.google.com/datastore")
+		@XmlJavaTypeAdapter(net.twisterrob.utils.datastore.KeyAdapter.class)
+		private com.google.appengine.api.datastore.Key key;
+
+		/**
+		 * This method is a gift to JAXB.
+		 */
+		@SuppressWarnings("unused")
+		private KeyHolder() {}
+
+		public KeyHolder(com.google.appengine.api.datastore.Key key) {
+			this.key = key;
+		}
 	}
 }
