7f37e27bb30ee90849f8cbe6b1574c01998bc060
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2013-11-23 16:29:28 +0000

    [SVN] r835 Cinema - Heroku in /Projects/Cinema/ [ADD] Heroku deploy

---
+++ Heroku/deploy.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Git\bin\sh.exe" -l -c "eval `ssh-agent` && ssh-add ~/.ssh/heroku_rsa && git add --all && git commit -m 'Deploy' && git push -f heroku master && heroku ps:scale web=1 && sh -l -i"
