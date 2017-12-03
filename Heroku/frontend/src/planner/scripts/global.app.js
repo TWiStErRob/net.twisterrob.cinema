2a0922e8dabb61729d4cf0610007328891ce989a
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2017-12-03 09:51:08 +0000

    [SVN] r2413 Cinema - Heroku in /Projects/Cinema/ [MOD] Bump Angular, Bootstrap, and Bootstrap-UI versions to latest [FIX] Breaking changes in Bootstrap-UI: uib-* components need a div [FIX] Breaking changes in Bootstrap: some classes were moved around [FIX] Breaking changes in Angular: filter: '!' is now filter: null, and filter: '!!' is now filter: {} [REM] Unused JSONP hack

--- Heroku/frontend/src/planner/scripts/global.app.js
+++ Heroku/frontend/src/planner/scripts/global.app.js
@@ -3,7 +3,7 @@ import 'angular'; // exports angular to global namespace
 import ngResource from 'angular-resource';
 import ngAnimate from 'angular-animate';
 
-import 'angular-ui-bootstrap/ui-bootstrap-tpls';
+import 'angular-ui-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import './ui-bootstrap-tpls-0.6+-confirm';
 
@@ -38,43 +38,4 @@ app.config(['$sceDelegateProvider', function($sceDelegateProvider) {
 		'self',
 		'http://webcache1.bbccustomerpublishing.com/cineworld/trailers/**'
 	]);
-}]);
-
-// https://github.com/angular/angular.js/issues/1551
-app.config([function fixCineworldCallbacks() {
-	var $window = window,
-	    callbacks = $window.angular.callbacks,
-	    pendingCallbacks = {},
-	    counter = callbacks.counter;
-	Object.defineProperty(callbacks, "counter", {
-		get: function() {
-			cleanFixedCallbacks();
-			var originalName = '_' + counter.toString(36);
-			var fixedName = ('angular.callbacks.' + originalName)
-					.replace(/[^a-zA-Z0-9_]/g, '');
-			function fixedCallback(data) {
-				callbacks[originalName](data);
-				delete $window[fixedName];
-				delete pendingCallbacks[fixedName];
-			}
-			fixedCallback.originalName = originalName;
-			$window[fixedName] = fixedCallback;
-			pendingCallbacks[fixedName] = fixedCallback;
-			return counter;
-		},
-		set: function(value) {
-			counter = value;
-		}
-	});
-
-	/**
-	 * Clear uncalled callback fixes.
-	 */
-	function cleanFixedCallbacks() {
-		angular.forEach(pendingCallbacks, function(fixedCallback, fqnStripped) {
-			if(!(fixedCallback.originalName in callbacks)) {
-				delete pendingCallbacks[fqnStripped];
-			}
-		});
-	}
 }]);
