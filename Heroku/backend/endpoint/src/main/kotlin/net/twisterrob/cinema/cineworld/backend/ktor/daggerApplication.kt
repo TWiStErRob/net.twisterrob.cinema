c1294e0ad97fb577f6573f3f7fa2c3d346ea3a81
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2020-02-15 19:40:33 +0000

    [SVN] r2947 Cinema - Heroku in /Projects/Cinema/ [EXP] Add dagger component as an item in Application.attributes

--- Heroku/backend/endpoint/src/main/kotlin/net/twisterrob/cinema/cineworld/backend/ktor/daggerApplication.kt
+++ Heroku/backend/endpoint/src/main/kotlin/net/twisterrob/cinema/cineworld/backend/ktor/daggerApplication.kt
@@ -1,8 +1,12 @@
 package net.twisterrob.cinema.cineworld.backend.ktor
 
 import io.ktor.application.Application
+import io.ktor.routing.Routing
+import io.ktor.routing.RoutingApplicationCall
+import io.ktor.util.AttributeKey
 import net.twisterrob.cinema.cineworld.backend.app.ApplicationComponent
 import net.twisterrob.cinema.cineworld.backend.app.DaggerApplicationComponent
+import net.twisterrob.cinema.cineworld.backend.ktor.ApplicationAttributes.dagger
 
 fun Application.daggerApplication() = daggerApplication(DaggerApplicationComponent::builder)
 
@@ -14,6 +18,28 @@ internal fun <DaggerComponentBuilder : ApplicationComponent.Builder> Application
 	builder.application(this)
 	initComponent(builder)
 	val dagger = builder.build()
+	this.dagger = dagger
 
 	dagger.controllers.register()
+}
+
+/**
+ * Attributes available for the [Application] related this app.
+ *
+ * **Note**: For most use cases this shouldn't be necessary, as everything is Dagger [Injected][javax.inject.Inject] automatically.
+ */
+object ApplicationAttributes {
+
+	private val DaggerComponent = AttributeKey<ApplicationComponent>("dagger")
+
+	var Application.dagger: ApplicationComponent
+		get() = attributes[DaggerComponent]
+		set(value) = attributes.put(DaggerComponent, value)
+
+	@Suppress("unused") // not needed yet, ApplicationComponent is alive for the whole Application lifecycle
+	private fun Application.registerDaggerForEachCall(dagger: ApplicationComponent) {
+		environment.monitor.subscribe(Routing.RoutingCallStarted) { call: RoutingApplicationCall ->
+			call.attributes.put(DaggerComponent, dagger)
+		}
+	}
 }
