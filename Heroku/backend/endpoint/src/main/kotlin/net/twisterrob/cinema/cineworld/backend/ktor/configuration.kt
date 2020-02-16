fa98a683797d8dd7e66bc19a41fb05ed9578065d
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2020-02-16 12:22:26 +0000

    [SVN] r2956 Cinema - Heroku in /Projects/Cinema/ [ADD] Error handling for exceptions to be shown in the browser

--- Heroku/backend/endpoint/src/main/kotlin/net/twisterrob/cinema/cineworld/backend/ktor/configuration.kt
+++ Heroku/backend/endpoint/src/main/kotlin/net/twisterrob/cinema/cineworld/backend/ktor/configuration.kt
@@ -2,12 +2,24 @@ package net.twisterrob.cinema.cineworld.backend.ktor
 
 import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.application.Application
+import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
+import io.ktor.features.StatusPages
+import io.ktor.html.respondHtml
+import io.ktor.http.HttpStatusCode
 import io.ktor.jackson.jackson
 import io.ktor.locations.Locations
+import kotlinx.html.body
+import kotlinx.html.h1
+import kotlinx.html.h2
+import kotlinx.html.head
+import kotlinx.html.pre
+import kotlinx.html.title
+import java.io.PrintWriter
+import java.io.StringWriter
 
 internal fun Application.configuration() {
 	install(DefaultHeaders)
@@ -16,6 +28,25 @@ internal fun Application.configuration() {
 	install(ContentNegotiation) {
 		jackson {
 			enable(SerializationFeature.INDENT_OUTPUT)
+		}
+	}
+	install(StatusPages) {
+		exception<Throwable> { cause ->
+			call.respondHtml(HttpStatusCode.InternalServerError) {
+				head {
+					title { +"Internal Server Error" }
+				}
+				body {
+					h1 {
+						+"Internal Server Error"
+					}
+					h2 { +"Exception" }
+					pre {
+						+StringWriter().apply { cause.printStackTrace(PrintWriter(this, true)) }.toString()
+					}
+				}
+			}
+			throw cause
 		}
 	}
 	install(Locations) // support @Location
