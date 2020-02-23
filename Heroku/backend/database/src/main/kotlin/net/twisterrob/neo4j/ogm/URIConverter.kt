f08db5f6912fff817f00818ced78bfea5fc194ee
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2020-02-23 02:51:45 +0000

    [SVN] r2997 Cinema - Heroku in /Projects/Cinema/ [FIX] Some films didn't load

--- Heroku/backend/database/src/main/kotlin/net/twisterrob/neo4j/ogm/URIConverter.kt
+++ Heroku/backend/database/src/main/kotlin/net/twisterrob/neo4j/ogm/URIConverter.kt
@@ -10,4 +10,12 @@ class URIConverter : AttributeConverter<URI, String> {
 	override fun toEntityAttribute(value: String?): URI? = value?.toURI()
 }
 
-private fun String.toURI(): URI = URI.create(this)
+// TODO workaround with replacement, make sure that data is always a valid URI during sync
+private fun String.toURI(): URI =
+	this
+		// In DB: http://webcache1.bbccustomerpublishing.com/cineworld/trailers/The Wolf of Wall Street_qtp.mp4
+		.replace(' ', '+')
+		// In DB: https://www.cineworld.co.uk/films/the-italian-job-[1969]-film-classics
+		.replace("[", "%5B")
+		.replace("]", "%5D")
+		.let { URI.create(it) }
