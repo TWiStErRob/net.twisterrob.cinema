53b03a84485a68a178250272fd9968a3069903a1
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2020-02-26 01:47:50 +0000

    [SVN] r3006 Cinema - Heroku in /Projects/Cinema/ [REF] Simplify performance combination code

--- Heroku/backend/endpoint/src/main/kotlin/net/twisterrob/cinema/cineworld/backend/endpoint/performance/data/GraphPerformanceRepository.kt
+++ Heroku/backend/endpoint/src/main/kotlin/net/twisterrob/cinema/cineworld/backend/endpoint/performance/data/GraphPerformanceRepository.kt
@@ -12,27 +12,17 @@ class GraphPerformanceRepository @Inject constructor(
 ) : PerformanceRepository {
 
 	override fun list(date: LocalDate, films: List<Long>, cinemas: List<Long>): List<Performances> =
-		combinations(date, films, cinemas).map {
-			val performances =
-				quickbookService.performances(date = it.date, cinema = it.cinema, film = it.film)
-			performanceMapper.map(date, it.cinema, it.film, performances)
-		}
-
-	private fun combinations(date: LocalDate, films: List<Long>, cinemas: List<Long>): List<PerformanceCombination> =
 		(cinemas x films).map { (cinema, film) ->
-			PerformanceCombination(date = date, cinema = cinema, film = film)
+			// TODO make these network calls in parallel (3)
+			val performances =
+				quickbookService.performances(date = date, cinema = cinema, film = film)
+			performanceMapper.map(date, cinema, film, performances)
 		}
-
-	private data class PerformanceCombination(
-		val date: LocalDate,
-		val cinema: Long,
-		val film: Long
-	)
 }
 
-private infix fun <T1, T2> Iterable<T1>.x(list2: Iterable<T2>): Iterable<Pair<T1, T2>> =
-	flatMap { item1 ->
-		list2.map { item2 ->
+private infix fun <T1, T2> Iterable<T1>.x(other: Iterable<T2>): Iterable<Pair<T1, T2>> =
+	this.flatMap { item1 ->
+		other.map { item2 ->
 			item1 to item2
 		}
 	}
