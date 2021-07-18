6b59f93e81d0978b1c11790cf2dc0d988d165a5d
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-07-18 01:50:39 +0100

    Stash Java 11

--- Heroku/build.gradle.kts
+++ Heroku/build.gradle.kts
@@ -36,17 +36,26 @@ allprojects {
 		val kapt = this@allprojects.extensions.getByName<KaptExtension>("kapt")
 		kapt.apply {
 			includeCompileClasspath = false
+//			javacOptions {
+//				option("-source", "8")
+//				option("-target", "8")
+//			}
 		}
 	}
 	plugins.withId("org.jetbrains.kotlin.jvm") {
 		tasks.withType<KotlinCompile> {
 			kotlinOptions {
-				jvmTarget = "1.8"
+				jvmTarget = JavaVersion.VERSION_1_8.toString()
+//				jvmTarget = JavaVersion.VERSION_11.toString()
 				freeCompilerArgs = freeCompilerArgs + "-Xuse-experimental=kotlin.Experimental"
 			}
 		}
 	}
 	plugins.withId("java") {
+//		configure<JavaPluginConvention> {
+//			sourceCompatibility = JavaVersion.VERSION_1_8
+//			targetCompatibility = JavaVersion.VERSION_11
+//		}
 		tasks.withType<Test> {
 			maxHeapSize = "512M"
 			//afterTest(KotlinClosure2({ descriptor: TestDescriptor, result: TestResult ->
