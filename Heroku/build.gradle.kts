46b6d71f90f0ae1c44a0fded7e211da00e972120
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-09-04 12:34:12 +0100

    Merge with existing configuration block.

--- Heroku/build.gradle.kts
+++ Heroku/build.gradle.kts
@@ -29,43 +29,19 @@ allprojects {
 	plugins.withId("java") {
 		tasks.withType<Test> {
 			maxHeapSize = "512M"
-			//afterTest(KotlinClosure2({ descriptor: TestDescriptor, result: TestResult ->
-			//	logger.quiet("Executing test ${descriptor.className}.${descriptor.name} with result: ${result.resultType}")
-			//}))
+			allowUnsafe()
+			if (project.property("net.twisterrob.build.verboseReports").toString().toBoolean()) {
+				configureVerboseReportsForGithubActions()
+			} else {
+				//afterTest(KotlinClosure2({ descriptor: TestDescriptor, result: TestResult ->
+				//	logger.quiet("Executing test ${descriptor.className}.${descriptor.name} with result: ${result.resultType}")
+				//}))
+			}
 		}
 
 		// JUnit 5 Tag setup, see JUnit5Tags.kt
 		@Suppress("UnstableApiUsage")
 		this@allprojects.tasks {
-			/**
-			 * https://github.com/neo4j/neo4j/issues/12712
-			 */
-			fun Test.allowUnsafe() {
-				if (JavaVersion.current() < JavaVersion.VERSION_1_9) return
-				// WARNING: Illegal reflective access using Lookup on org.neo4j.memory.RuntimeInternals
-				// (org.neo4j/neo4j-unsafe/4.2.0/neo4j-unsafe-4.2.0.jar)
-				// to class java.lang.String
-				jvmArgs("--add-opens", "java.base/java.lang=ALL-UNNAMED")
-				// WARNING: Illegal reflective access by org.apache.commons.lang3.reflect.FieldUtils
-				// (org.apache.commons/commons-lang3/3.11/commons-lang3-3.11.jar)
-				// to field sun.nio.ch.FileChannelImpl.positionLock
-				jvmArgs("--add-opens", "java.base/sun.nio.ch=ALL-UNNAMED")
-				// WARNING: Illegal reflective access by org.apache.commons.lang3.reflect.FieldUtils
-				// (org.apache.commons/commons-lang3/3.11/commons-lang3-3.11.jar)
-				// to field java.io.FileDescriptor.fd
-				jvmArgs("--add-opens", "java.base/java.io=ALL-UNNAMED")
-				// WARNING: Illegal reflective access by com.shazam.shazamcrest.CyclicReferenceDetector
-				// (com.shazam/shazamcrest/0.11/shazamcrest-0.11.jar)
-				// to field java.time.OffsetDateTime.serialVersionUID
-				// to field java.net.URI.serialVersionUID
-				jvmArgs("--add-opens", "java.base/java.time=ALL-UNNAMED")
-				jvmArgs("--add-opens", "java.base/java.net=ALL-UNNAMED")
-				// WARNING: Illegal reflective access by org.eclipse.collections.impl.utility.ArrayListIterate
-				// (org.eclipse.collections/eclipse-collections/10.3.0//eclipse-collections-10.3.0.jar)
-				// to field java.util.ArrayList.elementData
-				jvmArgs("--add-opens", "java.base/java.util=ALL-UNNAMED")
-			}
-
 			val test = "test"(Test::class) {
 				useJUnitPlatform {
 				}
@@ -104,13 +80,6 @@ allprojects {
 				dependsOn(integrationTest)
 				// Don't want to run it automatically, ever.
 				setDependsOn(dependsOn.filterNot { it is TaskProvider<*> && it.name == integrationExternalTest.name })
-			}
-
-			withType<Test> {
-				allowUnsafe()
-				if (project.property("net.twisterrob.build.verboseReports").toString().toBoolean()) {
-					configureVerboseReportsForGithubActions()
-				}
 			}
 		}
 	}
---
+++ Heroku/buildSrc/src/main/kotlin/allowUnsafe.kt
@@ -0,0 +1,31 @@
+import org.gradle.api.JavaVersion
+import org.gradle.api.tasks.testing.Test
+
+/**
+ * https://github.com/neo4j/neo4j/issues/12712
+ */
+fun Test.allowUnsafe() {
+	if (JavaVersion.current() < JavaVersion.VERSION_1_9) return
+	// WARNING: Illegal reflective access using Lookup on org.neo4j.memory.RuntimeInternals
+	// (org.neo4j/neo4j-unsafe/4.2.0/neo4j-unsafe-4.2.0.jar)
+	// to class java.lang.String
+	jvmArgs("--add-opens", "java.base/java.lang=ALL-UNNAMED")
+	// WARNING: Illegal reflective access by org.apache.commons.lang3.reflect.FieldUtils
+	// (org.apache.commons/commons-lang3/3.11/commons-lang3-3.11.jar)
+	// to field sun.nio.ch.FileChannelImpl.positionLock
+	jvmArgs("--add-opens", "java.base/sun.nio.ch=ALL-UNNAMED")
+	// WARNING: Illegal reflective access by org.apache.commons.lang3.reflect.FieldUtils
+	// (org.apache.commons/commons-lang3/3.11/commons-lang3-3.11.jar)
+	// to field java.io.FileDescriptor.fd
+	jvmArgs("--add-opens", "java.base/java.io=ALL-UNNAMED")
+	// WARNING: Illegal reflective access by com.shazam.shazamcrest.CyclicReferenceDetector
+	// (com.shazam/shazamcrest/0.11/shazamcrest-0.11.jar)
+	// to field java.time.OffsetDateTime.serialVersionUID
+	// to field java.net.URI.serialVersionUID
+	jvmArgs("--add-opens", "java.base/java.time=ALL-UNNAMED")
+	jvmArgs("--add-opens", "java.base/java.net=ALL-UNNAMED")
+	// WARNING: Illegal reflective access by org.eclipse.collections.impl.utility.ArrayListIterate
+	// (org.eclipse.collections/eclipse-collections/10.3.0//eclipse-collections-10.3.0.jar)
+	// to field java.util.ArrayList.elementData
+	jvmArgs("--add-opens", "java.base/java.util=ALL-UNNAMED")
+}
