36d20dc89ba3e97a5e24fbf9311fd7ced7225354
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-09-04 11:15:59 +0100

    Unify logging naming and style

--- Heroku/backend/sync/src/main/kotlin/net/twisterrob/cinema/cineworld/sync/FilmSync.kt
+++ Heroku/backend/sync/src/main/kotlin/net/twisterrob/cinema/cineworld/sync/FilmSync.kt
@@ -2,12 +2,11 @@ package net.twisterrob.cinema.cineworld.sync
 
 import net.twisterrob.cinema.cineworld.sync.syndication.FeedService
 import net.twisterrob.cinema.database.services.FilmService
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.time.OffsetDateTime
 import javax.inject.Inject
 
-private val log: Logger = LoggerFactory.getLogger(FilmSync::class.java)
+private val LOG = LoggerFactory.getLogger(FilmSync::class.java)
 
 class FilmSync @Inject constructor(
 	private val calculator: FilmSyncCalculator,
@@ -23,13 +22,13 @@ class FilmSync @Inject constructor(
 			feed = feedService.getWeeklyFilmTimes(),
 			dbFilms = dbService.findAll()
 		)
-		log.info(
+		LOG.info(
 			"Inserting {} new, updating {} existing ({} restored), deleting {} existing ({} already deleted) {}s for {}.",
 			sync.insert.size, sync.update.size, sync.restore.size, sync.delete.size, sync.alreadyDeleted.size,
 			"Film",
 			now
 		)
-		log.debug(sync.toString())
+		LOG.debug(sync.toString())
 		// re `justDeleted`: no actual delete, just added _deleted property
 		// re `restored`: no actual insert, just removed _deleted property
 		dbService.save(sync.insert + sync.update + sync.delete + sync.restore)
