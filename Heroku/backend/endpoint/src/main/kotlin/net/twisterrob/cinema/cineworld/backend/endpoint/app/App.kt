f433f08d56f9c1dfaa11d7cba518a558ae39bb73
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-09-04 10:23:12 +0100

    Reduce logging of network calls

--- Heroku/backend/endpoint/src/main/kotlin/net/twisterrob/cinema/cineworld/backend/endpoint/app/App.kt
+++ Heroku/backend/endpoint/src/main/kotlin/net/twisterrob/cinema/cineworld/backend/endpoint/app/App.kt
@@ -70,6 +70,8 @@ object App {
 	}
 }
 
+private class NetworkCall(url: String) : Exception("Callsite for $url")
+
 private class LevelLogger(
 	private val log: Logger,
 	private val logAtLevel: Logger.(message: String) -> Unit,
@@ -78,7 +80,18 @@ private class LevelLogger(
 	override fun log(message: String) {
 		if (message.startsWith("REQUEST: ")) {
 			val url = message.substringAfter("REQUEST: ")
-			log.info("Network call: $url", Exception("Network call callsite"))
+			log.info("Network call: $url", NetworkCall(url).apply {
+				stackTrace = stackTrace
+					.drop(1)
+					.filterNot {
+						it.className.startsWith("io.ktor.")
+								|| it.className.startsWith("kotlinx.coroutines.")
+								|| it.className.startsWith("io.netty.")
+								|| it.className.startsWith("kotlin.coroutines.")
+								|| it.className.startsWith("java.")
+					}
+					.toTypedArray()
+			})
 		}
 		log.logAtLevel(message)
 	}
