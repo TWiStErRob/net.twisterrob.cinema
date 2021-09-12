e6b17fd591ddde96f126d51ed11e65fe52da0db8
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-09-12 11:27:38 +0100

    Clean up webpack configuration

--- Heroku/frontend/webpack.config.js
+++ Heroku/frontend/webpack.config.js
@@ -2,72 +2,59 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // https://github.com/jantimon/html-webpack-plugin#options
 const CopyWebpackPlugin = require('copy-webpack-plugin'); // https://github.com/webpack-contrib/copy-webpack-plugin
 const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // https://github.com/webpack-contrib/mini-css-extract-plugin
-const Path = require('path');
+const path = require('path');
 
 module.exports = (env, argv) => {
-	const dist = Path.resolve(__dirname, '..', 'deploy');
+	const dist = path.resolve(__dirname, '..', 'deploy');
+	console.log(`Running in mode: ${argv.mode}; deploying to ${dist}.`);
+	const devMode = argv.mode === 'development';
 	return {
 		entry: {
-			'planner/index': './src/planner/scripts/index.js'
+			'planner/index': './src/planner/scripts/index.js',
 		},
 		output: {
-			path: Path.join(dist, 'static'),
+			path: path.join(dist, 'static'),
 			// don't set publicPath to an absolute path because it emits a <script> with that prefix
 			//publicPath: dist,
 			filename: '[name].js'
 		},
-		devtool: argv.mode === 'development'
+		devtool: devMode
 				? 'inline-source-map'
 				: undefined,
-		resolve: {
-			extensions: ['.js', '.json'/*, '.ts'*/]
-		},
 		module: {
 			// https://webpack.js.org/guides/migrating/#chaining-loaders
 			rules: [
-//			{
-//				test: /\.ts$/,
-//				use: ['ts-loader'],
-//				query: {
-//					ignoreDiagnostics: [
-//						2403, // 2403 -> Subsequent variable declarations
-//						2300, // 2300 -> Duplicate identifier
-//						2374, // 2374 -> Duplicate number index signature
-//						2375, // 2375 -> Duplicate string index signature
-//					]
-//				}
-//			},
-				{ test: /\.json$/, use: ['raw-loader'] },
-				{ test: /\.html$/, use: ['raw-loader'] },
 				{
-					test: /\.css$/,
+					test: /\.(html)$/,
+					// These templates/.html files are loaded into the $templateCache raw/as is.
+					include: [path.join(__dirname, 'src/planner/templates/')],
+					// When loaded with require() only asset/source gives contents.
+					// asset/resource gives url and asset/inline gives encoded data URI.
+					type: 'asset/source',
+				},
+				{
+					test: /\.(css)$/,
 					use: [
 						{
-							// https://webpack.js.org/plugins/mini-css-extract-plugin/
 							loader: MiniCssExtractPlugin.loader,
 						},
 						{
 							loader: 'css-loader',
 						},
-					]
+					],
 				},
 				{
 					test: /\.(sass|scss)/,
 					use: [
 						{
-							// https://webpack.js.org/plugins/mini-css-extract-plugin/
 							loader: MiniCssExtractPlugin.loader,
 						},
 						{
 							loader: 'css-loader',
-							options: {
-								sourceMap: true,
-							},
 						},
 						{
 							loader: 'sass-loader',
 							options: {
-								sourceMap: true,
 								sassOptions: {
 									outputStyle: 'expanded',
 								},
@@ -75,36 +62,39 @@ module.exports = (env, argv) => {
 						},
 					],
 				},
-				// TODO https://webpack.js.org/guides/asset-modules/
-				{ test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-					loader: 'url-loader',
-					options: {
-						outputPath: 'planner/',
-						name: '[contenthash]_[name].[ext]',
-						limit: 1000,
-						useRelativePath: true
-					}
+				{
+					test: /\.(png|jpg|gif)$/,
+					type: 'asset/resource',
+					generator: {
+						filename: 'images/[name]-[hash][ext][query]',
+					},
 				},
-			]
+				{
+					test: /\.(svg|eot|ttf|woff|woff2)$/,
+					type: 'asset/resource',
+					generator: {
+						filename: 'fonts/[name]-[hash][ext][query]',
+					},
+				},
+			],
 		},
 		plugins: [
 			new MiniCssExtractPlugin(),
 			new CopyWebpackPlugin({
 				patterns: [
-					{ from: 'src/old', to: 'planner-old' }
-				]
+					{ from: 'src/old', to: 'planner-old' },
+				],
 			}),
 			new HtmlWebpackPlugin({
 				chunks: [],
 				filename: 'index.html',
-				template: 'src/main/pages/index.html'
+				template: 'src/main/pages/index.html',
 			}),
 			new HtmlWebpackPlugin({
 				filename: 'planner/index.html',
 				chunks: ['planner/index'],
-				inject: 'head', // temporary workaround while all dependencies are migrated
-				template: 'src/planner/pages/index.html'
-			})
-		]
+				template: 'src/planner/pages/index.html',
+			}),
+		],
 	};
 };
