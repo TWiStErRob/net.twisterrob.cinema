ff5f9ad85be0ab5549ddf8d04e943567fbf43f56
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-09-13 22:03:26 +0100

    Add screenings to parameters

--- Heroku/backend/sync/src/main/kotlin/net/twisterrob/cinema/cineworld/sync/MainParameters.kt
+++ Heroku/backend/sync/src/main/kotlin/net/twisterrob/cinema/cineworld/sync/MainParameters.kt
@@ -3,4 +3,5 @@ package net.twisterrob.cinema.cineworld.sync
 data class MainParameters(
 	val syncCinemas: Boolean,
 	val syncFilms: Boolean,
+	val syncScreenings: Boolean,
 )
--- Heroku/backend/sync/src/main/kotlin/net/twisterrob/cinema/cineworld/sync/MainParametersParser.kt
+++ Heroku/backend/sync/src/main/kotlin/net/twisterrob/cinema/cineworld/sync/MainParametersParser.kt
@@ -3,12 +3,16 @@ package net.twisterrob.cinema.cineworld.sync
 class MainParametersParser {
 
 	fun parse(vararg args: String): MainParameters {
+		val syncCinemas = "cinemas" in args
+		val syncFilms = "films" in args
+		val syncScreenings = "screenings" in args
 		val params = MainParameters(
-			syncCinemas = "cinemas" in args,
-			syncFilms = "films" in args,
+			syncCinemas = syncCinemas || syncScreenings,
+			syncFilms = syncFilms || syncScreenings,
+			syncScreenings = syncScreenings
 		)
-		require(params.syncCinemas || params.syncFilms) {
-			"""Must have "cinemas" or "films" or both as an argument."""
+		require(params.syncCinemas || params.syncFilms || params.syncScreenings) {
+			"""Must have "cinemas" or "films" or "screenings" or any/all as an argument."""
 		}
 		return params
 	}
