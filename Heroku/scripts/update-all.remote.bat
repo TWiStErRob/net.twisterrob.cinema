583d6a763b7f1da8123e31003dac687989338cc3
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-09-16 22:17:39 +0100

    Update everything on remote

--- Heroku/scripts/update-all.remote.bat
+++ Heroku/scripts/update-all.remote.bat
@@ -1,1 +1,1 @@
-heroku run --app twisterrob-cinema java -jar backend/sync/build/libs/twisterrob-cinema-backend-sync.jar films cinemas
+heroku run --app twisterrob-cinema java -jar backend/sync/build/libs/twisterrob-cinema-backend-sync.jar cinemas films performances
