bc8429144bff82108de4b258a29c65f80b92b3ff
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-09-20 22:28:27 +0100

    how many?

--- Heroku/buildSrc/src/main/kotlin/parallelJUnit5Execution.kt
+++ Heroku/buildSrc/src/main/kotlin/parallelJUnit5Execution.kt
@@ -10,6 +10,8 @@ fun Test.parallelJUnit5Execution(concurrency: Concurrency) {
 	// Split up work into as many processes as possible to ease memory pressure for each executor.
 	// But not as a workaround for https://github.com/gradle/gradle/issues/6453, as it is not necessary since Gradle 5.1.
 	maxParallelForks = Runtime.getRuntime().availableProcessors() / 2 + 1
+	println(Runtime.getRuntime().availableProcessors())
+	println(maxParallelForks)
 
 	systemProperty("junit.jupiter.testinstance.lifecycle.default", "per_method")
 	systemProperty("junit.jupiter.execution.parallel.config.strategy", "dynamic")
