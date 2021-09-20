2ff13611a15b440b31435620d6a24037f492ff89
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2021-09-20 21:23:20 +0100

    Make sure logging is available in all tests

--- Heroku/backend/network/build.gradle.kts
+++ Heroku/backend/network/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
 	implementation(Deps.Kotlin.core)
 	implementation(Deps.Ktor.client.client)
 	implementation(Deps.Ktor.client.logging_jvm)
-	Deps.Log4J2.slf4j(project)
+	implementation(Deps.SLF4J.core)
 }
 
 // Test
@@ -18,4 +18,5 @@ dependencies {
 	testImplementation(Deps.Ktor.client.mock_jvm)
 	testImplementation(Deps.Ktor.client.logging_jvm)
 	testImplementation(project(":test-helpers"))
+	Deps.Log4J2.slf4jForTest(project)
 }
