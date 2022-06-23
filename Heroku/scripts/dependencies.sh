76bf6ebbbf31842caf270cbdb2529380bd77fbf7
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-06-23 18:16:33 +0100

    code review

--- Heroku/scripts/dependencies.sh
+++ Heroku/scripts/dependencies.sh
@@ -2,35 +2,35 @@
 ./gradlew --stacktrace --version
 
 # Pure outputs
-#echo 'allDependencies'
-#./gradlew --stacktrace allDependencies > dependencies.txt
+echo 'allDependencies'
+./gradlew --stacktrace :allDependencies > dependencies.txt
 
-#echo ':dependencies'
-#./gradlew --stacktrace :dependencies > dependencies-root.txt
+echo ':dependencies'
+./gradlew --stacktrace :dependencies > dependencies-root.txt
 
-#echo ':backend:dependencies'
-#./gradlew --stacktrace :backend:dependencies > dependencies-backend.txt
+echo ':backend:dependencies'
+./gradlew --stacktrace :backend:dependencies > dependencies-backend.txt
 
-#echo ':backend:sync:dependencies'
-#./gradlew --stacktrace :backend:sync:dependencies > dependencies-backend-sync.txt
+echo ':backend:sync:dependencies'
+./gradlew --stacktrace :backend:sync:dependencies > dependencies-backend-sync.txt
 
-#echo ':backend:feed:dependencies'
-#./gradlew --stacktrace :backend:feed:dependencies > dependencies-backend-feed.txt
+echo ':backend:feed:dependencies'
+./gradlew --stacktrace :backend:feed:dependencies > dependencies-backend-feed.txt
 
-#echo ':backend:quickbook:dependencies'
-#./gradlew --stacktrace :backend:quickbook:dependencies > dependencies-backend-quickbook.txt
+echo ':backend:quickbook:dependencies'
+./gradlew --stacktrace :backend:quickbook:dependencies > dependencies-backend-quickbook.txt
 
-#echo ':backend:database:dependencies'
-#./gradlew --stacktrace :backend:database:dependencies > dependencies-backend-database.txt
+echo ':backend:database:dependencies'
+./gradlew --stacktrace :backend:database:dependencies > dependencies-backend-database.txt
 
-#echo ':backend:network:dependencies'
-#./gradlew --stacktrace :backend:network:dependencies > dependencies-backend-network.txt
+echo ':backend:network:dependencies'
+./gradlew --stacktrace :backend:network:dependencies > dependencies-backend-network.txt
 
 echo ':backend:endpoint:dependencies'
 ./gradlew --stacktrace :backend:endpoint:dependencies > dependencies-backend-endpoint.txt
 
-#echo ':test-helpers:dependencies'
-#./gradlew --stacktrace :test-helpers:dependencies > dependencies-test-helpers.txt
+echo ':test-helpers:dependencies'
+./gradlew --stacktrace :test-helpers:dependencies > dependencies-test-helpers.txt
 
 # Teardown
 echo 'Stopping daemon'
