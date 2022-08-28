ff4f40ea1b212630dcf78c2ad353a4bd523cbf78
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-08-28 20:53:14 +0100

    Rename files

--- Heroku/scripts/dependencies.sh
+++ Heroku/scripts/dependencies.sh
@@ -3,34 +3,34 @@
 
 # Pure outputs
 echo 'allDependencies'
-./gradlew --stacktrace :allDependencies --write-locks >dependencies.txt 2>&1
+./gradlew --stacktrace :allDependencies --write-locks >all.dependencies 2>&1
 
 echo ':dependencies'
-./gradlew --stacktrace :dependencies >dependencies-root.txt 2>&1
+./gradlew --stacktrace :dependencies >root.dependencies 2>&1
 
 echo ':backend:dependencies'
-./gradlew --stacktrace :backend:dependencies >dependencies-backend.txt 2>&1
+./gradlew --stacktrace :backend:dependencies >backend.dependencies 2>&1
 
 echo ':backend:sync:dependencies'
-./gradlew --stacktrace :backend:sync:dependencies >dependencies-backend-sync.txt 2>&1
+./gradlew --stacktrace :backend:sync:dependencies >backend-sync.dependencies 2>&1
 
 echo ':backend:feed:dependencies'
-./gradlew --stacktrace :backend:feed:dependencies >dependencies-backend-feed.txt 2>&1
+./gradlew --stacktrace :backend:feed:dependencies >backend-feed.dependencies 2>&1
 
 echo ':backend:quickbook:dependencies'
-./gradlew --stacktrace :backend:quickbook:dependencies >dependencies-backend-quickbook.txt 2>&1
+./gradlew --stacktrace :backend:quickbook:dependencies >backend-quickbook.dependencies 2>&1
 
 echo ':backend:database:dependencies'
-./gradlew --stacktrace :backend:database:dependencies >dependencies-backend-database.txt 2>&1
+./gradlew --stacktrace :backend:database:dependencies >backend-database.dependencies 2>&1
 
 echo ':backend:network:dependencies'
-./gradlew --stacktrace :backend:network:dependencies >dependencies-backend-network.txt 2>&1
+./gradlew --stacktrace :backend:network:dependencies >backend-network.dependencies 2>&1
 
 echo ':backend:endpoint:dependencies'
-./gradlew --stacktrace :backend:endpoint:dependencies >dependencies-backend-endpoint.txt 2>&1
+./gradlew --stacktrace :backend:endpoint:dependencies >backend-endpoint.dependencies 2>&1
 
 echo ':test-helpers:dependencies'
-./gradlew --stacktrace :test-helpers:dependencies >dependencies-test-helpers.txt 2>&1
+./gradlew --stacktrace :test-helpers:dependencies >test-helpers.dependencies 2>&1
 
 # Teardown
 echo 'Stopping daemon'
