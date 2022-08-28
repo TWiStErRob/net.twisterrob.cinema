cbb61376bc6a720b0fb7a64415fa749886b6abe4
Róbert Papp <papp.robert.s@gmail.com>
2022-08-28 17:34:16 +0100

    Migrate buildSrc to includeBuild in settings.gradle.kts (#201)

--- Heroku/gradle/plugins/settings.gradle.kts
+++ Heroku/gradle/plugins/settings.gradle.kts
@@ -1,10 +1,8 @@
-rootProject.name = "Cinema-Heroku-buildSrc"
-
 dependencyResolutionManagement {
 	@Suppress("UnstableApiUsage")
 	versionCatalogs {
 		create("libs") {
-			from(files("../gradle/libs.versions.toml"))
+			from(files("../libs.versions.toml"))
 		}
 	}
 }
