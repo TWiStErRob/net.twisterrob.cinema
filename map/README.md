3b5b1d425e79bd288a3ab034baea0b1c652a8cc6
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-09-10 20:09:03 +0100

    Cineworld KML map generator

---
+++ map/README.md
@@ -0,0 +1,3 @@
+Found this [map by someone else](https://www.google.com/maps/@54.4749724,-4.1148165,6.24z/data=!4m2!6m1!1s1yBKNTNQqk2pWlN9Y66EJNhUlbrg).
+
+But it was old, so I wanted to create [my own](https://www.google.com/maps/d/viewer?mid=1lSuA4BePXoKkBVywDFvcDqcqls_S-_E&ll=53.35241195250196%2C-2.2997229999999824&z=6).
---
+++ map/map.main.kts
@@ -0,0 +1,238 @@
+// Based on:
+// main.kts: https://kotlinlang.org/docs/custom-script-deps-tutorial.html
+// HttpClient: https://ktor.io/docs/http-client-engines.html#java
+// Jackson: https://ktor.io/docs/serialization-client.html > Jackson
+// jackson {} config: https://www.baeldung.com/jackson-deserialize-json-unknown-properties
+// Kml lib: https://web.archive.org/web/20170317215912/http://labs.micromata.de/projects/jak/quickstart.html
+// Kml format: exported an example from [My Maps](https://www.google.com/maps/d).
+
+// Implicit dependency: Java 11 (because ktor 2.x is compiled as Class 55)
+// Note: normally these dependencies are listed without a -jvm suffix,
+// but there's no Gradle resolution in play here, so we have to pick a platform manually.
+@file:Repository("https://repo1.maven.org/maven2/")
+@file:DependsOn("org.jetbrains.kotlinx:kotlinx-html-jvm:0.8.0")
+@file:DependsOn("io.ktor:ktor-client-java-jvm:2.1.1")
+@file:DependsOn("io.ktor:ktor-client-content-negotiation-jvm:2.1.1")
+@file:DependsOn("io.ktor:ktor-serialization-jackson-jvm:2.1.1")
+// Override transitively included jaxb-impl:2.2 to avoid warning when marshalling Kml.
+// > Illegal reflective access by com.sun.xml.bind.v2.runtime.reflect.opt.Injector$1 (jaxb-impl-2.2.jar)
+// > to method java.lang.ClassLoader.defineClass(java.lang.String,byte[],int,int)
+@file:DependsOn("com.sun.xml.bind:jaxb-impl:2.3.6")
+@file:DependsOn("de.micromata.jak:JavaAPIforKml:2.2.1")
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import de.micromata.opengis.kml.v_2_2_0.Document
+import de.micromata.opengis.kml.v_2_2_0.Kml
+import de.micromata.opengis.kml.v_2_2_0.Point
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.jackson.jackson
+import kotlinx.coroutines.runBlocking
+import kotlinx.html.a
+import kotlinx.html.body
+import kotlinx.html.html
+import kotlinx.html.img
+import kotlinx.html.p
+import kotlinx.html.stream.createHTML
+import java.awt.Color
+import java.awt.Graphics
+import java.awt.image.BufferedImage
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.net.URI
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
+import javax.imageio.IIOImage
+import javax.imageio.ImageIO
+import javax.imageio.ImageWriteParam
+import javax.imageio.ImageWriter
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
+fun ObjectMapper.cineworldConfig() {
+	configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+}
+
+val client = HttpClient {
+	install(ContentNegotiation) {
+		jackson {
+			cineworldConfig()
+		}
+	}
+	expectSuccess = true
+}
+
+data class CinemasResponse(
+	val body: CinemasBody,
+) {
+
+	data class CinemasBody(
+		val cinemas: List<Cinema>,
+	) {
+
+		data class Cinema(
+			val id: String,
+			val groupId: String,
+			val displayName: String,
+			val link: URI,
+			val address: String,
+			val imageUrl: URI,
+			val latitude: Double,
+			val longitude: Double,
+		)
+	}
+}
+
+fun loadFile(): List<CinemasResponse.CinemasBody.Cinema> {
+	val client = jacksonObjectMapper().apply { cineworldConfig() }.reader()
+	return client.readValue(File("cinemas.json"), CinemasResponse::class.java).body.cinemas
+}
+
+fun loadNetwork(now: LocalDate): List<CinemasResponse.CinemasBody.Cinema> {
+	fun cinemas(url: String): List<CinemasResponse.CinemasBody.Cinema> {
+		println("Loading... ${url}")
+		return runBlocking { client.get(url).body<CinemasResponse>().body.cinemas }
+	}
+
+	val nextYear = now.plusYears(1).toString()
+	val uk =
+		cinemas("https://www.cineworld.co.uk/uk/data-api-service/v1/quickbook/10108/cinemas/with-event/until/${nextYear}?attr=&lang=en_GB")
+	val ie =
+		cinemas("https://www.cineworld.ie/ie/data-api-service/v1/quickbook/10109/cinemas/with-event/until/${nextYear}?attr=&lang=en_IE")
+	return uk + ie
+}
+
+val now = LocalDate.now()
+val cinemas = loadFile()
+//println(cinemas.map { it.displayName })
+val kml = Kml().apply {
+	feature = Document().apply {
+		name = "Cineworld Cinemas"
+		val niceNow = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).format(now)
+		description = "List of all the cineworld cinemas in the UK on ${niceNow}."
+		createAndAddStyle().apply {
+			id = "cineworld-placemark"
+			createAndSetIconStyle().apply {
+				scale = 1.0
+				createAndSetIcon().apply {
+					href = "https://www.google.com/s2/favicons?domain=www.cineworld.co.uk"
+				}
+			}
+		}
+		createAndAddFolder().apply {
+			name = "Cineworld Cinemas"
+			cinemas.forEach { cinema ->
+				createAndAddPlacemark().apply {
+					name = cinema.displayName
+					description = createHTML(prettyPrint = false).html {
+						body {
+							p {
+								+"Website: "
+								a(href = cinema.link.toString(), target = "_blank") { +cinema.link.toString() }
+							}
+							img(src = "images/cinema-${cinema.id}.jpg", alt = cinema.displayName)
+							p {
+								+"Address: "
+								+cinema.address
+							}
+						}
+					}.toString()
+					address = cinema.address
+					geometry = Point().addToCoordinates(cinema.longitude, cinema.latitude)
+					styleUrl = "#cineworld-placemark"
+				}
+			}
+		}
+	}
+}
+
+val kmlFile = File("cinemas.kml").apply {
+	delete()
+	kml.marshal(this)
+	writeText(readText().replace("ns2:", "").replace("xmlns:ns2", "xmlns"))
+}
+//print(kmlFile.readText())
+val kmzFile = File("cinemas.kmz").apply {
+	delete()
+	ZipOutputStream(FileOutputStream(this)).use { zip ->
+		fun file(name: String, content: ByteArray) {
+			zip.putNextEntry(ZipEntry(name))
+			zip.write(content)
+			zip.closeEntry()
+		}
+		file("doc.kml", kmlFile.readBytes())
+		cinemas.forEach { cinema ->
+			println("Saving image... ${cinema.id}")
+			val originalImage = runBlocking<ByteArray> { client.get(cinema.imageUrl.toString()).body() }
+			file("images/cinema-${cinema.id}.jpg", compress(originalImage))
+		}
+	}
+}
+println(kmzFile.absolutePath)
+
+/**
+ * Convert each source PNG image to a JPG image, because Google My Maps has a 5MB limit on KMZ.
+ * Based on [StackOverflow](https://stackoverflow.com/a/57571061/253468]).
+ */
+fun compress(originalImage: ByteArray): ByteArray {
+	val memoryImage = removeAlphaChannel(ImageIO.read(ByteArrayInputStream(originalImage)))
+
+	ImageIO.getImageWritersByFormatName("jpg").next().use { writer ->
+		val jpgWriteParam = writer.defaultWriteParam.apply {
+			compressionMode = ImageWriteParam.MODE_EXPLICIT
+			compressionQuality = 0.5f
+		}
+
+		val bytes = ByteArrayOutputStream()
+		writer.output = ImageIO.createImageOutputStream(bytes)
+		writer.write(null, IIOImage(memoryImage, null, null), jpgWriteParam)
+		return bytes.toByteArray()
+	}
+}
+
+fun removeAlphaChannel(img: BufferedImage): BufferedImage {
+	if (!img.colorModel.hasAlpha()) {
+		return img
+	}
+	return BufferedImage(img.width, img.height, BufferedImage.TYPE_INT_RGB).apply {
+		createGraphics().use {
+			it.color = Color.WHITE
+			it.fillRect(0, 0, img.width, img.height)
+			it.drawImage(img, 0, 0, null)
+		}
+	}
+}
+
+@OptIn(ExperimentalContracts::class)
+inline fun <R> ImageWriter.use(block: (ImageWriter) -> R): R {
+	contract {
+		callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+	}
+	try {
+		return block(this)
+	} finally {
+		dispose()
+	}
+}
+
+@OptIn(ExperimentalContracts::class)
+inline fun <R> Graphics.use(block: (Graphics) -> R): R {
+	contract {
+		callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+	}
+	try {
+		return block(this)
+	} finally {
+		dispose()
+	}
+}
