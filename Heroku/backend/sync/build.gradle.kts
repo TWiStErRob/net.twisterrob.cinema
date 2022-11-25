791a50f07ade6860e1c22f8a25ebd0199e82df07
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-11-25 18:39:06 +0000

    Aaargh

--- Heroku/backend/sync/build.gradle.kts
+++ Heroku/backend/sync/build.gradle.kts
@@ -11,7 +11,7 @@ application {
 	mainClass.set("net.twisterrob.cinema.cineworld.sync.Main")
 	tasks.named<JavaExec>("run") {
 		jvmArgs(
-			if (property("net.twisterrob.run.verboseSync").toString().toBoolean())
+			if (project.property("net.twisterrob.run.verboseSync").toString().toBoolean())
 				"-Dlog4j.configurationFile=log4j2.xml"
 			else
 				"-Dlog4j.configurationFile=log4j2.xml,log4j2-sync.xml"
