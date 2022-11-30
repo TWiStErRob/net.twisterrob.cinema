5fb1327fa5d151efc51bfb96a818ec07235354dd
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2022-11-30 19:02:18 +0000

    Revise

--- Heroku/gradle/plugins/src/main/kotlin/allowUnsafe.kt
+++ Heroku/gradle/plugins/src/main/kotlin/allowUnsafe.kt
@@ -21,6 +21,11 @@ fun Test.allowUnsafe() {
 	// WARNING: Illegal reflective access using Lookup on org.neo4j.internal.unsafe.UnsafeUtil
 	// (org.neo4j/neo4j-unsafe/4.4.12/neo4j-unsafe-4.4.12.jar)
 	// to class java.nio.Buffer
+	//
+	// See https://github.com/neo4j/neo4j/issues/12932
+	// java.lang.IllegalAccessException: module java.base does not open java.nio to unnamed module @7a35b0f5
+	// at java.base/java.lang.invoke.MethodHandles.privateLookupIn(MethodHandles.java:259)
+	// at org.neo4j.internal.unsafe.UnsafeUtil.<clinit>(UnsafeUtil.java:111)
 	addOpens("java.base/java.nio")
 	// WARNING: Illegal reflective access by com.shazam.shazamcrest.CyclicReferenceDetector
 	// (com.shazam/shazamcrest/0.11/shazamcrest-0.11.jar)
@@ -34,11 +39,6 @@ fun Test.allowUnsafe() {
 	// (org.eclipse.collections/eclipse-collections/10.3.0//eclipse-collections-10.3.0.jar)
 	// to field java.util.ArrayList.elementData
 	addOpens("java.base/java.util")
-	// See https://github.com/neo4j/neo4j/issues/12932
-	// java.lang.IllegalAccessException: module java.base does not open java.nio to unnamed module @7a35b0f5
-	// at java.base/java.lang.invoke.MethodHandles.privateLookupIn(MethodHandles.java:259)
-	// at org.neo4j.internal.unsafe.UnsafeUtil.<clinit>(UnsafeUtil.java:111)
-	addOpens("java.base/java.nio")
 }
 
 private fun Test.addOpens(module: String) {
