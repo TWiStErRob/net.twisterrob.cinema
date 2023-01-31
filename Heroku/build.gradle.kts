e851602c235ba4a6e9fd1a7319511d99f3a97326
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-01-31 15:07:24 +0000

    Suppress deprecations

--- Heroku/build.gradle.kts
+++ Heroku/build.gradle.kts
@@ -172,3 +172,35 @@ project.tasks.create<TestReport>("allTestsReport") {
 		}
 	}
 }
+
+val gradleVersion: String = GradleVersion.current().version
+
+// TODEL Gradle sync in IDEA 2022.3.1: https://youtrack.jetbrains.com/issue/IDEA-306975
+@Suppress("MaxLineLength")
+doNotNagAbout(
+	"The AbstractArchiveTask.archivePath property has been deprecated. " +
+			"This is scheduled to be removed in Gradle 9.0. " +
+			"Please use the archiveFile property instead. " +
+			"See https://docs.gradle.org/${gradleVersion}/dsl/org.gradle.api.tasks.bundling.AbstractArchiveTask.html#org.gradle.api.tasks.bundling.AbstractArchiveTask:archivePath for more details.",
+	"at org.jetbrains.plugins.gradle.tooling.builder.ExternalProjectBuilderImpl\$_getSourceSets_closure"
+)
+
+// TODEL Gradle sync in IDEA 2022.3.1: https://youtrack.jetbrains.com/issue/IDEA-306975
+@Suppress("MaxLineLength")
+doNotNagAbout(
+	"The AbstractArchiveTask.archivePath property has been deprecated. " +
+			"This is scheduled to be removed in Gradle 9.0. " +
+			"Please use the archiveFile property instead. " +
+			"See https://docs.gradle.org/${gradleVersion}/dsl/org.gradle.api.tasks.bundling.AbstractArchiveTask.html#org.gradle.api.tasks.bundling.AbstractArchiveTask:archivePath for more details.",
+	"at org.jetbrains.plugins.gradle.tooling.util.SourceSetCachedFinder.createArtifactsMap"
+)
+
+// TODEL AppEngine 2.4.5 https://github.com/GoogleCloudPlatform/app-gradle-plugin/issues/446
+@Suppress("MaxLineLength")
+doNotNagAbout(
+	"The AbstractArchiveTask.archivePath property has been deprecated. " +
+			"This is scheduled to be removed in Gradle 9.0. " +
+			"Please use the archiveFile property instead. " +
+			"See https://docs.gradle.org/${gradleVersion}/dsl/org.gradle.api.tasks.bundling.AbstractArchiveTask.html#org.gradle.api.tasks.bundling.AbstractArchiveTask:archivePath for more details.",
+	"at com.google.cloud.tools.gradle.appengine.appyaml.AppEngineAppYamlPlugin.lambda\$configureExtensions$0"
+)
