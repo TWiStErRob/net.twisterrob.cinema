6e85768e414ab9119d62a251a8f2a6aa3f84bb8a
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-04-20 22:40:03 +0100

    Fix deprecations and remove workaround for https://github.com/ktorio/ktor/issues/514

--- Heroku/backend/endpoint/src/main/kotlin/net/twisterrob/cinema/cineworld/backend/endpoint/app/AppController.kt
+++ Heroku/backend/endpoint/src/main/kotlin/net/twisterrob/cinema/cineworld/backend/endpoint/app/AppController.kt
@@ -3,13 +3,10 @@ package net.twisterrob.cinema.cineworld.backend.endpoint.app
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
 import io.ktor.server.application.log
-import io.ktor.server.http.content.default
-import io.ktor.server.http.content.files
-import io.ktor.server.http.content.static
+import io.ktor.server.http.content.staticFiles
 import io.ktor.server.http.content.staticRootFolder
 import io.ktor.server.response.respondRedirect
 import io.ktor.server.routing.Routing
-import io.ktor.server.routing.application
 import io.ktor.server.routing.get
 import net.twisterrob.cinema.cineworld.backend.ktor.RouteController
 import net.twisterrob.cinema.cineworld.backend.ktor.staticRootFolder
@@ -33,8 +30,7 @@ class AppController @Inject constructor(
 			call.respondRedirect("https://www.google.com/s2/favicons?domain=www.cineworld.co.uk")
 		}
 
-		static("/") {
-			staticRootFolder = application.environment.config.staticRootFolder
+		staticFiles("/", application.environment.config.staticRootFolder) {
 			application.log.debug(
 				"""
 					Running static content at / from ${staticRootFolder}
@@ -42,19 +38,6 @@ class AppController @Inject constructor(
 					  -> ${staticRootFolder?.canonicalPath}
 				""".trimIndent()
 			)
-			// Serve all files from staticRootFolder
-			files(".")
-			// `/` = `/index.html`
-			default("index.html")
-
-			// workaround for https://github.com/ktorio/ktor/issues/514#issuecomment-414982574
-			// > `default()` function is not applied to subdirectories
-			static("planner") {
-				@Suppress("UnsafeCallOnNullableType") // staticRootFolder is set above, so assumption holds for now.
-				staticRootFolder = parent!!.staticRootFolder!!.resolve("planner")
-				files(".")
-				default("index.html")
-			}
 		}
 	}
 }
