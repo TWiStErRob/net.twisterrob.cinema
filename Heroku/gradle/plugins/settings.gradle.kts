f42a63c29735793fd045f7aaf7e436861190730a
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-05-13 19:01:04 +0100

    Allow qualified names

--- Heroku/gradle/plugins/settings.gradle.kts
+++ Heroku/gradle/plugins/settings.gradle.kts
@@ -25,12 +25,13 @@ buildscript {
 
 val gradleVersion: String = GradleVersion.current().version
 
+// The resolvable usage is already allowed on configuration ':plugins:generatePrecompiledScriptPluginAccessors:accessors8939604837258708605:testFixturesRuntimeClasspath'. This behavior has been deprecated. This behavior is scheduled to be removed in Gradle 9.0. Remove the call to setCanBeResolved(true), it has no effect. Consult the upgrading guide for further information: https://docs.gradle.org/8.2-milestone-1/userguide/upgrading_version_8.html#redundant_configuration_usage_activation
 // TODEL Gradle 8.2 vs Kotlin 1.8.20 https://github.com/gradle/gradle/pull/24271#issuecomment-1546706115
 @Suppress("MaxLineLength")
 doNotNagAbout(
 	Regex(
 		Regex.escape("The resolvable usage is already allowed on configuration ") +
-				"':generatePrecompiledScriptPluginAccessors:accessors\\d+:testFixturesRuntimeClasspath'. " +
+				"'(:plugins)?:generatePrecompiledScriptPluginAccessors:accessors\\d+:testFixturesRuntimeClasspath'. " +
 				Regex.escape("This behavior has been deprecated. ") +
 				Regex.escape("This behavior is scheduled to be removed in Gradle 9.0. ") +
 				Regex.escape("Remove the call to setCanBeResolved(true), it has no effect. ") +
