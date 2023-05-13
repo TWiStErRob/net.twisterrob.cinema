dc8a1f3c10cf71b32f7aece5e3dd45558106c084
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-05-13 18:50:01 +0100

    Suppress plugins separately

--- Heroku/gradle/plugins/settings.gradle.kts
+++ Heroku/gradle/plugins/settings.gradle.kts
@@ -1,3 +1,9 @@
+import net.twisterrob.gradle.doNotNagAbout
+
+plugins {
+	id("net.twisterrob.gradle.plugin.settings") version "0.15.1"
+}
+
 dependencyResolutionManagement {
 	repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
 	repositories {
@@ -16,3 +22,23 @@ buildscript {
 		lockFile = file("../../gradle/dependency-locks/plugins-settings.lockfile")
 	}
 }
+
+val gradleVersion: String = GradleVersion.current().version
+
+// TODEL Gradle 8.2 vs Kotlin 1.8.20 https://github.com/gradle/gradle/pull/24271#issuecomment-1546706115
+@Suppress("MaxLineLength")
+doNotNagAbout(
+	Regex(
+		Regex.escape("The resolvable usage is already allowed on configuration ") +
+				"':generatePrecompiledScriptPluginAccessors:accessors\\d+:testFixturesRuntimeClasspath'. " +
+				Regex.escape("This behavior has been deprecated. ") +
+				Regex.escape("This behavior is scheduled to be removed in Gradle 9.0. ") +
+				Regex.escape("Remove the call to setCanBeResolved(true), it has no effect. ") +
+				Regex.escape("Consult the upgrading guide for further information: ") +
+				Regex.escape("https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_8.html#redundant_configuration_usage_activation") +
+				".*" +
+				// Task :generatePrecompiledScriptPluginAccessors 
+				Regex.escape("at org.jetbrains.kotlin.gradle.plugin.mpp.compilationImpl.factory.KotlinCompilationDependencyConfigurationsFactoriesKt.KotlinCompilationDependencyConfigurationsContainer") +
+				".*"
+	)
+)
