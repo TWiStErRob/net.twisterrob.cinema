3edc555661b099a7f486be9a5103baf7c3b650fb
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-07-25 11:32:03 +0100

    Use new plugin

--- Heroku/settings.gradle.kts
+++ Heroku/settings.gradle.kts
@@ -37,7 +37,7 @@ pluginManagement {
 
 plugins {
 	id("net.twisterrob.cinema.settings")
-	id("net.twisterrob.gradle.plugin.settings") version "0.16"
+	id("net.twisterrob.gradle.plugin.nagging") version "0.16"
 }
 
 dependencyResolutionManagement {
