ca69a1c69bf59752782950d6017c8d3ef9ab6805
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-08-02 20:21:53 +0100

    Don't log to file, JUnit will capture stdout, so it'll be in context!

--- Heroku/test-integration/build.gradle.kts
+++ Heroku/test-integration/build.gradle.kts
@@ -31,14 +31,4 @@ tasks.integrationTest.configure {
 	// TODEL https://github.com/gradle/gradle/issues/861
 	properties.forEach { (name, value) -> inputs.property(name, value) }
 	properties.forEach { (name, value) -> value?.let { systemProperty(name, value) } }
-
-	if (!providers.systemProperty("idea.paths.selector").isPresent) {
-		logger.info("Writing logs to a file instead of console.")
-		// TODO when tests run in parallel, sessions will overwrite each other.
-		val logFile = layout.buildDirectory.file("logs/integrationTest-selenium.log")
-		doFirst {
-			logFile.get().asFile.parentFile.mkdirs()
-		}
-		systemProperty("webdriver.chrome.logfile", logFile.get().asFile.absolutePath)
-	}
 }
