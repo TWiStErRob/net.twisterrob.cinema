4b2e356294b13da3ef483a945d0e6b52331c2778
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-08-03 23:51:52 +0100

    Move from hacky representation to using a configuration to set up custom toStrings.
    This helps "Expecting all elements of:
                 [FQCN@hashCode, FQCN@hashCode] to also become formatted.

--- Heroku/test-integration/src/main/kotlin/net/twisterrob/cinema/frontend/test/framework/AssertJ.kt
+++ Heroku/test-integration/src/main/kotlin/net/twisterrob/cinema/frontend/test/framework/AssertJ.kt
@@ -1,5 +1,6 @@
 package net.twisterrob.cinema.frontend.test.framework
 
+import org.assertj.core.api.AbstractAssert
 import org.assertj.core.api.Assert
 import org.assertj.core.util.CheckReturnValue
 import kotlin.reflect.KFunction1
@@ -14,88 +15,95 @@ fun <A : Assert<*, *>> A.verify(block: A.() -> Unit): A {
 }
 
 /**
- * Has to use `(String, Array<out Any?>) -> Unit` instead of `(String, vararg Any?) -> Nothing` for `failWithMessage`.
- *  * vararg is not possible in function type
- *  * failWithMessage is defined in Java, so there's no Nothing type, and :: doesn't auto-convert it.
+ * Has to return [Unit] instead of [Nothing] for `failWithMessage`, because
+ * `failWithMessage` is defined in Java, so there's no Nothing type, and :: doesn't auto-convert it.
  */
 private typealias ActuallyNothing = Unit
 
 @CheckReturnValue
-fun <A : Assert<out Assert<A, T>, T>, T> A.check(
+fun <A : AbstractAssert<out Assert<A, T>, T>, T> A.check(
 	propertyName: String,
 	actual: T,
 	property: KProperty1<T, Boolean>,
-	failWithMessage: (String, Array<out Any?>) -> ActuallyNothing,
+	failWithMessage: (String) -> ActuallyNothing,
 ): A = verify {
 	if (!property(actual)) {
-		failWithMessage("Expected element to be ${propertyName}. But was not!", emptyArray())
+		val actualAsString = info.representation().toStringOf(actual)
+		failWithMessage("Expected ${actualAsString} to be ${propertyName}. But was not!")
 		failWithMessageReturnsNothing()
 	}
 }
 
 @CheckReturnValue
-fun <A : Assert<out Assert<A, T>, T>, T> A.checkNot(
+fun <A : AbstractAssert<out Assert<A, T>, T>, T> A.checkNot(
 	propertyName: String,
 	actual: T,
 	property: KProperty1<T, Boolean>,
-	failWithMessage: (String, Array<out Any?>) -> ActuallyNothing,
+	failWithMessage: (String) -> ActuallyNothing,
 ): A = verify {
 	if (property(actual)) {
-		failWithMessage("Expected element to not be ${propertyName}. But was!", emptyArray())
+		val actualAsString = info.representation().toStringOf(actual)
+		failWithMessage("Expected ${actualAsString} to not be ${propertyName}. But was!")
 		failWithMessageReturnsNothing()
 	}
 }
 
 @CheckReturnValue
-fun <A : Assert<out Assert<A, T>, T>, T> A.check(
+fun <A : AbstractAssert<out Assert<A, T>, T>, T> A.check(
 	propertyName: String,
 	actual: T,
 	property: KFunction1<T, Boolean>,
-	failWithMessage: (String, Array<out Any?>) -> ActuallyNothing,
+	failWithMessage: (String) -> ActuallyNothing,
 ): A = verify {
 	if (!property(actual)) {
-		failWithMessage("Expected element to be ${propertyName}. But was not!", emptyArray())
+		val actualAsString = info.representation().toStringOf(actual)
+		failWithMessage("Expected ${actualAsString} to be ${propertyName}. But was not!")
 		failWithMessageReturnsNothing()
 	}
 }
 
 @CheckReturnValue
-fun <A : Assert<out Assert<A, T>, T>, T> A.checkNot(
+fun <A : AbstractAssert<out Assert<A, T>, T>, T> A.checkNot(
 	propertyName: String,
 	actual: T,
 	property: KFunction1<T, Boolean>,
-	failWithMessage: (String, Array<out Any?>) -> ActuallyNothing,
+	failWithMessage: (String) -> ActuallyNothing,
 ): A = verify {
 	if (property(actual)) {
-		failWithMessage("Expected element to not be ${propertyName}. But was!", emptyArray())
+		val actualAsString = info.representation().toStringOf(actual)
+		failWithMessage("Expected ${actualAsString} to not be ${propertyName}. But was!")
 		failWithMessageReturnsNothing()
 	}
 }
 
 @CheckReturnValue
-fun <A : Assert<out Assert<A, T>, T>, T, P> A.checkHas(
+fun <A : AbstractAssert<out Assert<A, T>, T>, T, P> A.checkHas(
 	propertyName: String,
 	actual: T,
 	property: KFunction2<T, P, Boolean>,
 	arg: P,
-	failWithMessage: (String, Array<out Any?>) -> ActuallyNothing,
+	failWithMessage: (String) -> ActuallyNothing,
 ): A = verify {
 	if (!property(actual, arg)) {
-		failWithMessage("Expected element to have ${propertyName} ${arg ?: "null"}. But did not have!", emptyArray())
+		val actualAsString = info.representation().toStringOf(actual)
+		val argAsString = info.representation().toStringOf(arg)
+		failWithMessage("Expected ${actualAsString} to have ${propertyName} ${argAsString}. But did not have!")
 		failWithMessageReturnsNothing()
 	}
 }
 
 @CheckReturnValue
-fun <A : Assert<out Assert<A, T>, T>, T, P> A.checkDoesNotHave(
+fun <A : AbstractAssert<out Assert<A, T>, T>, T, P> A.checkDoesNotHave(
 	propertyName: String,
 	actual: T,
 	property: KFunction2<T, P, Boolean>,
 	arg: P,
-	failWithMessage: (String, Array<out Any?>) -> ActuallyNothing,
+	failWithMessage: (String) -> ActuallyNothing,
 ): A = verify {
 	if (property(actual, arg)) {
-		failWithMessage("Expected element to not have ${propertyName} ${arg ?: "null"}. But had!", emptyArray())
+		val actualAsString = info.representation().toStringOf(actual)
+		val argAsString = info.representation().toStringOf(arg)
+		failWithMessage("Expected ${actualAsString} to not have ${propertyName} ${argAsString}. But had!")
 		failWithMessageReturnsNothing()
 	}
 }
