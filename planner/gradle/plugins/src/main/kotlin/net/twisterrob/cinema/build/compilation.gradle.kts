30dbef796bb36cb603ef89fa61ff7b150e0e9f99
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2025-09-12 23:30:25 +0100

    opt in

--- planner/gradle/plugins/src/main/kotlin/net/twisterrob/cinema/build/compilation.gradle.kts
+++ planner/gradle/plugins/src/main/kotlin/net/twisterrob/cinema/build/compilation.gradle.kts
@@ -21,5 +21,14 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 
 		// Enable flag to help suppress compiler warnings.
 		freeCompilerArgs.add("-Xrender-internal-diagnostic-names")
+
+		// Kotlin 2.2: ANNOTATION_WILL_BE_APPLIED_ALSO_TO_PROPERTY_OR_FIELD
+		// > This annotation is currently applied to the value parameter only,
+		// > but in the future it will also be applied to field.
+		// > - To opt in to applying to both value parameter and field,
+		// >   add '-Xannotation-default-target=param-property' to your compiler arguments.
+		// > - To keep applying to the value parameter only, use the '@param:' annotation target.
+		// > See https://youtrack.jetbrains.com/issue/KT-73255 for more details.
+		freeCompilerArgs.add("-Xannotation-default-target=param-property")
 	}
 }
