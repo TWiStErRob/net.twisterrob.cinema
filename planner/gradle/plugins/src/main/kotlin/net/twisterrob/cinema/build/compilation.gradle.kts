46b7d1d2b9117071b8977fb59b1fd282654e0121
Róbert Papp <papp.robert.s@gmail.com>
2025-09-16 20:41:28 +0100

    Update Kotlin language 2.0 to 2.2 (#1122)

--- planner/gradle/plugins/src/main/kotlin/net/twisterrob/cinema/build/compilation.gradle.kts
+++ planner/gradle/plugins/src/main/kotlin/net/twisterrob/cinema/build/compilation.gradle.kts
@@ -2,7 +2,6 @@ package net.twisterrob.cinema.build
 
 import net.twisterrob.cinema.build.dsl.libs
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 plugins {
 	id("org.gradle.java")
@@ -14,15 +13,22 @@ java {
 	targetCompatibility = JavaVersion.toVersion(libs.versions.java.get())
 }
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+kotlin {
 	compilerOptions {
 		jvmTarget = JvmTarget.fromTarget(libs.versions.java.get())
 		allWarningsAsErrors = true
 		verbose = true
 
-		languageVersion = KotlinVersion.KOTLIN_2_0
-
 		// Enable flag to help suppress compiler warnings.
 		freeCompilerArgs.add("-Xrender-internal-diagnostic-names")
+
+		// Kotlin 2.2: ANNOTATION_WILL_BE_APPLIED_ALSO_TO_PROPERTY_OR_FIELD
+		// > This annotation is currently applied to the value parameter only,
+		// > but in the future it will also be applied to field.
+		// > - To opt in to applying to both value parameter and field,
+		// >   add '-Xannotation-default-target=param-property' to your compiler arguments.
+		// > - To keep applying to the value parameter only, use the '@param:' annotation target.
+		// > See https://youtrack.jetbrains.com/issue/KT-73255 for more details.
+		freeCompilerArgs.add("-Xannotation-default-target=param-property")
 	}
 }
