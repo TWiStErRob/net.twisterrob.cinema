e54f2f10e6c3f78fe4814694c0fef498a54ea31b
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2025-09-28 21:53:15 +0100

    separate logic

--- planner/test-helpers/src/main/kotlin/net/twisterrob/test/neo4j/neo4jContainer.kt
+++ planner/test-helpers/src/main/kotlin/net/twisterrob/test/neo4j/neo4jContainer.kt
@@ -1,11 +1,11 @@
 package net.twisterrob.test.neo4j
 
+import net.twisterrob.test.readManifestEntry
 import org.neo4j.driver.Driver
 import org.neo4j.driver.GraphDatabase
 import org.neo4j.driver.Session
 import org.testcontainers.containers.Neo4jContainer
 import org.testcontainers.utility.DockerImageName
-import java.net.JarURLConnection
 import java.net.URI
 
 private val NEO4J_VERSION: String by lazy {
@@ -26,15 +26,3 @@ fun <R> Neo4jContainer<*>.session(block: (Session).() -> R): R =
 	this.createDriver().use { driver ->
 		driver.session().use(block)
 	}
-
-private fun readManifestEntry(name: String): String {
-	val aClassInJar = object {}.javaClass
-	val aClassName = aClassInJar.enclosingClass.simpleName + ".class"
-	val res = aClassInJar.getResource(aClassName) ?: error("Cannot find class file ${aClassName}")
-	val url = res.openConnection() ?: error("Cannot open ${res}")
-	url as? JarURLConnection ?: error("Unsupported packaging mechanism: ${url}, no JAR file to get manifest from.")
-	val mf = url.manifest ?: error("Cannot find manifest in ${url.jarFileURL}")
-	val version = mf.mainAttributes.getValue(name)
-		?: error("${name} attribute not present in manifest\n${url.manifest.mainAttributes.toMap()}")
-	return version
-}
