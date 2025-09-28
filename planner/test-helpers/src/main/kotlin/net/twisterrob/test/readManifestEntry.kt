34e12289e183d846f7638c10788cfeeab039efe1
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2025-09-28 21:57:03 +0100

    doc

--- planner/test-helpers/src/main/kotlin/net/twisterrob/test/readManifestEntry.kt
+++ planner/test-helpers/src/main/kotlin/net/twisterrob/test/readManifestEntry.kt
@@ -2,9 +2,13 @@ package net.twisterrob.test
 
 import java.net.JarURLConnection
 
-private val READ_MANIFEST_ENTRY_KT = object {}.javaClass.enclosingClass
-
-internal fun readManifestEntry(name: String, aClassInJar: Class<*> = READ_MANIFEST_ENTRY_KT): String {
+/**
+ * Reads an entry from `META-INF/MANIFEST.MF`.
+ *
+ * @param aClassInJar the default value should be copied to all callsites,
+ * resulting in the value being read from the right JAR.
+ */
+internal fun readManifestEntry(name: String, aClassInJar: Class<*> = object {}.javaClass.enclosingClass): String {
 	val aClassName = aClassInJar.simpleName + ".class"
 	val res = aClassInJar.getResource(aClassName) ?: error("Cannot find class file ${aClassName}")
 	val url = res.openConnection() ?: error("Cannot open ${res}")
