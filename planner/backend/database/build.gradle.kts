d1da45d7ab9e14ba2b021dd7981f3dd3391f9cb6
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2025-12-07 12:08:10 +0000

    remove direct neo4j dep

--- planner/backend/database/build.gradle.kts
+++ planner/backend/database/build.gradle.kts
@@ -32,7 +32,6 @@ dependencies {
 	testImplementation(enforcedPlatform("com.google.code.gson:gson:2.8.9"))
 	testImplementation(projects.testHelpers)
 
-	testImplementation(libs.neo4j)
 	testImplementation(libs.neo4j.harness)
 
 	Deps.slf4jToLog4jForTest(project)
