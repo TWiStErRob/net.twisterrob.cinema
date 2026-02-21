a7dfdc814f1bd9132f8d7bd03f1cf6cf5b9918eb
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2026-02-21 02:37:28 +0000

    Replace deprecated intercept with createRouteScopedPlugin

--- planner/backend/endpoint/src/main/kotlin/net/twisterrob/cinema/cineworld/backend/endpoint/app/TestController.kt
+++ planner/backend/endpoint/src/main/kotlin/net/twisterrob/cinema/cineworld/backend/endpoint/app/TestController.kt
@@ -1,24 +1,12 @@
 package net.twisterrob.cinema.cineworld.backend.endpoint.app
 
-import io.ktor.http.HttpHeaders
-import io.ktor.http.encodeURLPath
 import io.ktor.server.application.Application
-import io.ktor.server.application.ApplicationCallPipeline
-import io.ktor.server.application.PipelineCall
-import io.ktor.server.application.call
 import io.ktor.server.application.log
-import io.ktor.server.request.path
-import io.ktor.server.request.uri
-import io.ktor.server.response.header
-import io.ktor.server.response.respondFile
 import io.ktor.server.routing.Routing
-import io.ktor.server.routing.intercept
-import io.ktor.util.pipeline.PipelineContext
 import net.twisterrob.cinema.cineworld.backend.ktor.Env
 import net.twisterrob.cinema.cineworld.backend.ktor.RouteController
 import net.twisterrob.cinema.cineworld.backend.ktor.environment
 import net.twisterrob.cinema.cineworld.backend.ktor.fakeRootFolder
-import java.io.File
 import javax.inject.Inject
 
 class TestController @Inject constructor(
@@ -39,32 +27,8 @@ class TestController @Inject constructor(
 				  -> ${root.canonicalPath}
 			""".trimIndent()
 		)
-		@Suppress("DEPRECATION") // STOPSHIP
-		intercept(ApplicationCallPipeline.Call) {
-			val fullPathAndQuery = this.call.request.uri.ending
-			val fakeFullPathAndQueryFile = root.resolve(fullPathAndQuery)
-			if (fakeFullPathAndQueryFile.exists()) {
-				respondFake(fakeFullPathAndQueryFile)
-				return@intercept
-			}
-
-			val fullPath = this.call.request.path().ending
-			val fakeFullPathFile = root.resolve(fullPath)
-			if (fakeFullPathFile.exists()) {
-				respondFake(fakeFullPathFile)
-				return@intercept
-			}
-
-			// no fake found, respond normally
+		install(FakeContentPlugin) {
+			this.root = root
 		}
 	}
-
-	private suspend fun PipelineContext<Unit, PipelineCall>.respondFake(path: File) {
-		call.application.log.warn("Fake response to ${call.request.uri} with ${path.canonicalPath}")
-		call.response.header(HttpHeaders.XForwardedServer, "fakes")
-		call.respondFile(path)
-	}
 }
-
-private val String.ending: String
-	get() = substringAfter("/").encodeURLPath().ifBlank { "index.html" }
---
+++ planner/backend/endpoint/src/main/kotlin/net/twisterrob/cinema/cineworld/backend/endpoint/auth/AuthSessionPlugin.kt
@@ -0,0 +1,40 @@
+package net.twisterrob.cinema.cineworld.backend.endpoint.auth
+
+import io.ktor.server.application.PipelineCall
+import io.ktor.server.application.createRouteScopedPlugin
+import io.ktor.server.application.log
+import io.ktor.server.sessions.clear
+import io.ktor.server.sessions.get
+import io.ktor.server.sessions.sessions
+import net.twisterrob.cinema.cineworld.backend.app.ApplicationAttributes.currentUser
+import net.twisterrob.cinema.cineworld.backend.endpoint.auth.data.AuthSession
+import net.twisterrob.cinema.cineworld.backend.endpoint.auth.data.CurrentUser
+import net.twisterrob.cinema.cineworld.backend.endpoint.auth.data.UnknownUserException
+import net.twisterrob.cinema.cineworld.backend.endpoint.auth.data.User
+
+internal val AuthSessionPlugin = createRouteScopedPlugin(
+	name = "AuthSessionPlugin",
+	createConfiguration = ::AuthSessionPluginConfig,
+) {
+	onCall { call ->
+		val findUser = requireNotNull(pluginConfig.findUser) { "Missing way to retrieve user" }
+		call.setupCurrentUserFromSession(findUser)
+	}
+}
+
+private fun PipelineCall.setupCurrentUserFromSession(findUser: (String) -> User) {
+	val session: AuthSession? = this.sessions.get()
+	if (session != null) {
+		try {
+			val user = findUser(session.userId)
+			this.attributes.currentUser = CurrentUser(id = user.id, email = user.email)
+		} catch (ex: UnknownUserException) {
+			this.application.log.error("Invalid session: {}", this.sessions, ex)
+			this.sessions.clear<AuthSession>()
+		}
+	}
+}
+
+internal class AuthSessionPluginConfig(
+	var findUser: ((userId: String) -> User)? = null,
+)
