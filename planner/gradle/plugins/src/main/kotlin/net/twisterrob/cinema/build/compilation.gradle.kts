92021857aa0fc81c6de29aea63ea508572875264
Róbert Papp <papp.robert.s@gmail.com>
2026-09-20 13:20:02 +0100

    Update compilation.gradle.kts

--- planner/gradle/plugins/src/main/kotlin/net/twisterrob/cinema/build/compilation.gradle.kts
+++ planner/gradle/plugins/src/main/kotlin/net/twisterrob/cinema/build/compilation.gradle.kts
@@ -19,19 +19,7 @@ kotlin {
 		allWarningsAsErrors = true
 		verbose = true
 
-		// Opt in to Context Parameters.
-		freeCompilerArgs.add("-Xcontext-parameters")
-
 		// Enable flag to help suppress compiler warnings.
 		freeCompilerArgs.add("-Xrender-internal-diagnostic-names")
-
-		// Kotlin 2.2: ANNOTATION_WILL_BE_APPLIED_ALSO_TO_PROPERTY_OR_FIELD
-		// > This annotation is currently applied to the value parameter only,
-		// > but in the future it will also be applied to field.
-		// > - To opt in to applying to both value parameter and field,
-		// >   add '-Xannotation-default-target=param-property' to your compiler arguments.
-		// > - To keep applying to the value parameter only, use the '@param:' annotation target.
-		// > See https://youtrack.jetbrains.com/issue/KT-73255 for more details.
-		freeCompilerArgs.add("-Xannotation-default-target=param-property")
 	}
 }
